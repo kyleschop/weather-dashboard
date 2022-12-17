91c106fafb356cd9fd40cfbefe11d663a53e5234
Kyle Schoppmann <klparuu@yahoo.com>
2022-12-17 14:08:26 -0700

    Added funtionality to save searched cities as a button

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,23 @@ var city = $('input[name="searchCity"]');
 var searchBtn = $('.searchBtn');
 var searchSectionEl = $('#searchSection');
 var searchedCity = [];
+var storedCity = JSON.parse(localStorage.getItem("searchedCity"));
+
+function makeCityBtn() {
+    var cityBtn = $('<button>');
+    cityBtn.text(' ');
+    for (var i = 0; i < searchedCity.length; i++) {
+        var potatoe = JSON.parse(localStorage.getItem("searchedCity"));
+    
+            if (potatoe[i] !== null) {
+                cityBtn.css('margin-top', '5px');
+                cityBtn.text(potatoe[i]);
+            }
+        searchSectionEl.append(cityBtn);
+    } 
+};
 
 function saveCity() {
-    var storedCity = JSON.parse(localStorage.getItem("searchedCity"));
-
     if(storedCity === null) {
         searchedCity = searchedCity.concat(city.val());
         localStorage.setItem("searchedCity", JSON.stringify(searchedCity));
@@ -19,6 +32,7 @@ function saveCity() {
 function search() {
     console.log(city.val());
     saveCity();
+    makeCityBtn();
 };
 
 searchBtn.on('click', search);
